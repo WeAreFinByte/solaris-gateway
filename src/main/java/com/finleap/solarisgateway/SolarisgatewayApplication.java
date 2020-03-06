5f6d4d663c0a1c216ef200230d11836bf2dbebf8
Yunus Bayhan <yunus.bayhan@finbyte.com>
2020-03-06 10:00:49 +0100

    SBG-01
    Changes
    ----
    * initial commit

---
+++ src/main/java/com/finleap/solarisgateway/SolarisgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.finleap.solarisgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarisgatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolarisgatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finleap/solarisgateway/SolarisgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finleap.solarisgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarisgatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
