37a8f924d963d42af00eaab2d818eb4cd656ed97
Ali Onur Geven <alionur.geven@finbyte.com>
2020-03-16 15:06:14 +0100

    Fix docker-compose job exit

--- integration-tests/entrypoint.sh
+++ integration-tests/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+err_report() {
+    echo "Error on line $1"
+}
+
+trap 'err_report $LINENO' ERR
+
 wait-for-url() {
     while ! curl http://uaa:8080/healthz
     do
@@ -13,7 +19,7 @@ wait-for-url() {
 
 wait-for-url
 
-response=$(curl --write-out %{http_code} --silent --output /dev/null http://gatewasdasay:8090/uaa/header)
+response=$(curl --write-out %{http_code} --silent --output /dev/null http://gateway:8090/uaa/header)
 
 if [[ "$response" -ne 200 ]] ; then
   echo "Site status changed to $response"
